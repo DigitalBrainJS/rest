9de04fed8db1b5a9a327c066ce4c17cd1d00f48d
Dmitriy Mozgovoy <robotshara@gmail.com>
2019-12-03 20:26:07 +0200

    allowUnknown of author route changed to false

--- routes/authors.js
+++ routes/authors.js
@@ -39,7 +39,7 @@ router
             }, {
                 fields: parseArray
             },
-            {allowUnknown: true}),
+            {allowUnknown: false}),
         accept.path({
             id: idValidator
         }),
